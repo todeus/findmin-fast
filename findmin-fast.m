cf8cf3fd796a7d66e11fd30112c53083f986b838
todeus <gtodeus@gmail.com>
2010-03-03 15:32:16 +1000

    Метод наскорейшего градиентного спуска

---
+++ findmin-fast.m
@@ -0,0 +1,18 @@
+n = 3;
+C = rand(n,n);
+A = C'*C;
+b = rand(n,1);
+X = rand(n,1);
+e = 0.00001;
+%L = 1 / (max(eig(A))+1);
+F_ = 1;
+i=0;
+while max(abs(F_)) > e
+    F_ = 2*A*X+b;
+    L = (X'*A*F_+F_'*A*X+F_'*b)/(2*F_'*A*F_);
+    Xn = X - F_ * L;
+    X = Xn;
+    i=i+1;
+endwhile;
+F_
+i
